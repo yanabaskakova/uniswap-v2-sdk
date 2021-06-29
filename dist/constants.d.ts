202a0fd67e975a61af3214fa4436ad179e829cc7
yanabaskakova <baskakovayana@gmail.com>
2021-06-29 22:26:37 +0300

    change factory address

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x9b3fEF64b1Aa1144B04f46F8119F2C51c8fD0D1F";
+export declare const INIT_CODE_HASH = "0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
